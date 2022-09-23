9c1fc3c6226162f35de31d4a0d8db855378b3809
Raul <raul.lara.unam@gmail.com>
2022-09-22 19:44:05 -0500

    Se agrega disenio responsive

--- main-page-demo-react/src/components/ContentMainPage.jsx
+++ main-page-demo-react/src/components/ContentMainPage.jsx
@@ -1,4 +1,5 @@
 import imgMain from '../images/image-hero-desktop.png';
+import imgMobile from '../images/image-hero-mobile.png';
 import imgDatabiz from '../images/client-databiz.svg';
 import imgAudiophile from '../images/client-audiophile.svg';
 import imgMeet from '../images/client-meet.svg';
@@ -8,31 +9,41 @@ import imgMaker from '../images/client-maker.svg';
 export const ContentMainPage = () => {
     return (
         <div className="main-page-hero">
+
+            <div className="main-page-img-container" id="check-mobile">
+                <img className="main-page-img-mobile" src={imgMobile} alt="hero-sktop" />
+            </div>
+
             <div className="main-page-hero-children">
-                <h1 className="main-page-h1">Make <br />remote work</h1>
                 
-                <div className="main-page-text">
-                    <span>
-                        Get your team in sync, no matter your location. 
-                        <br /> Streamline processes, create team rituals, and
-                        <br /> watch productivity soar.
-                    </span>
-                </div>
+                    <h1 className="main-page-h1">Make remote work</h1>
+
+                    <div className="main-page-text">
+                        <span>
+                            Get your team in sync, no matter your location.
+                             Streamline processes, create team rituals, and
+                             watch productivity soar.
+                        </span>
+                    </div>
+
+                    <div>
+                        <button className="main-page-btn-learn-more">Learn more</button>
+                    </div>
+
+                    <div>
+                        <img className="main-page-client-logo" src={imgDatabiz} alt="Databiz" />
+                        <img className="main-page-client-logo" src={imgAudiophile} alt="Audiophile" />
+                        <img className="main-page-client-logo" src={imgMeet} alt="Meet" />
+                        <img className="main-page-client-logo" src={imgMaker} alt="Maker" />
+                    </div>
+
                 
-                <div>
-                    <button className="main-page-btn-learn-more">Learn more</button>  
-                </div>
+            </div>
 
-                <div>
-                    <img className="main-page-client-logo" src={ imgDatabiz } alt="Databiz" />
-                    <img className="main-page-client-logo" src={ imgAudiophile } alt="Audiophile" />
-                    <img className="main-page-client-logo" src={ imgMeet } alt="Meet" />
-                    <img className="main-page-client-logo" src={ imgMaker } alt="Maker" />
-                </div>
-
-            </div>            
-
-            <img className="main-page-img" src={imgMain} alt="hero-desktop" />
+            <div className="main-page-img-container" id="check-desktop">
+                <img className="main-page-img" src={imgMain} alt="hero-desktop" />
+            </div>
+            
         </div>
     )
 }
